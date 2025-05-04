4c6a491b6082a252d2f821a6e181fa0060331dd1
Ram <rgrouthu@gmail.com>
2025-05-04 10:49:19 +0530

    parallel exection changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   expect:{
     timeout:10_000
   },
-  reporter:'html',
+  reporter: [['html'], ['junit', { outputFile: 'test-results/results.xml' }]],
+
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
